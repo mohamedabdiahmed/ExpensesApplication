88b3999a47a361f01505a89bb8dcc8d91a9e47c0
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2023-03-05 23:59:15 +0300

    Done

---
+++ Controllers/userController.js
@@ -0,0 +1,22 @@
+const {prismaClient } = require('@prisma/client');
+const prisma = new  prismaClient();
+
+const getAllUsers = async(req,res,next)=>{
+    const {userName,email,password} = req.body;
+    const users = await prisma.Users.findMany({
+        where:{
+            userName,
+            email,
+            password
+        }
+    });
+    try {
+        res.status(202).json({
+            users
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: `Xog kuma jirto`
+        })
+    }
+}
---
+++ Routes/userRoutes.js
@@ -0,0 +1,6 @@
+const Express = require('express');
+const router = Express.Router();
+
+router.get('/');
+
+module.exports = router;
